f94209625f9af2c5f14d13b9d197c44db7a2a7c3
Romeo Martin Rosales <romeomartinrosales@gmail.com>
2024-12-08 06:24:16 +0000

    Modularized Sections

---
+++ app/components/bottom-banner.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+export default function BottomBanner() {
+    return <>
+        <section className="banner-wrapper" id="sectionHire">
+            <div className="bottom-CTA-container">
+                <div className="banner-block">
+                    <div className="hero-mini-photo">
+                        <img src="./images/hero-mini-photo.webp" alt="" />
+                    </div>
+                    <p className="highlight">Romeo Martin Rosales ⚡ Product Designer + Developer</p>
+                    <h1>Let's work together.
+                    </h1>
+                    <p>Collaborate with me to transform your ideas into impactful designs that make a difference.</p>
+                    <div className="cta-set-wrapper">
+                        <a className="cta" id="linkedin-cta" href="https://www.linkedin.com/in/takierosales"
+                            target="_blank">Connect with me on LinkedIn</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Footer from "./components/footer";
 import Projects from "./components/projects-sect";
 import About from "./components/about-sect";
 import Reviews from "./components/review-sect";
+import BottomBanner from "./components/bottom-banner";
 
 export default function Page() {
     return <>
@@ -16,24 +17,7 @@ export default function Page() {
             <Projects />
             <About />
             <Reviews />
-
-            <section className="banner-wrapper" id="sectionHire">
-                <div className="bottom-CTA-container">
-                    <div className="banner-block">
-                        <div className="hero-mini-photo">
-                            <img src="./images/hero-mini-photo.webp" alt="" />
-                        </div>
-                        <p className="highlight">Romeo Martin Rosales ⚡ Product Designer + Developer</p>
-                        <h1>Let's work together.
-                        </h1>
-                        <p>Collaborate with me to transform your ideas into impactful designs that make a difference.</p>
-                        <div className="cta-set-wrapper">
-                            <a className="cta" id="linkedin-cta" href="https://www.linkedin.com/in/takierosales"
-                                target="_blank">Connect with me on LinkedIn</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <BottomBanner />
         </main>
         <Footer />
 
